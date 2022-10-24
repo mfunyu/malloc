d43e2e51a3540a11ff42e2fba5e00fd68c482e31
mfunyu <mfunyu@student.42lyon.fr>
2022-10-24 15:17:28 +0900

    update: printf in all funcs

--- srcs/free.c
+++ srcs/free.c
@@ -1,7 +1,7 @@
 #include "libft.h"
+#include <stdio.h> //TODO: delete
 
 void free(void *ptr)
 {
-
-
+	printf("malloc: ptr = %p\n", ptr);
 }
--- srcs/realloc.c
+++ srcs/realloc.c
@@ -1,7 +1,8 @@
 #include "libft.h"
+#include <stdio.h> //TODO: delete
 
 void *realloc(void *ptr, size_t size)
 {
-
-
+	printf("malloc: ptr = %p, size = %zu\n", ptr, size);
+	return (NULL);
 }
