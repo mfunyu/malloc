8530657809196cf8162044138a5347399bc5bf41
mfunyu <mfunyu@student.42tokyo.jp>
2023-01-22 16:47:09 +0900

    add: working header/footer model

--- malloc.h
+++ malloc.h
@@ -3,6 +3,18 @@
 
 # include <stdlib.h>
 
+# define WORD 4
+# define DWORD 8
+
+# define PUT(ptr, value) *ptr = value
+
+# define PACK(size, alloc) (size | alloc)
+# define SIZE(ptr) (*(unsigned int *)(ptr - WORD) & ~0x7)
+
+# define HEADER(ptr) (unsigned int *)(ptr - WORD)
+# define FOOTER(ptr) (unsigned int *)(ptr + SIZE(ptr) + WORD)
+# define NEXTPTR(ptr) ptr + SIZE(ptr) + DWORD
+
 void	free(void *ptr);
 void	*malloc(size_t size);
 void	*realloc(void *ptr, size_t size);
