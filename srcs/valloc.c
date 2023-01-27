0188da9f823bb26553524d3e89325452a3409e00
mfunyu <mfunyu@student.42lyon.fr>
2023-01-27 17:21:11 +0900

    add: calloc / valloc file

---
+++ srcs/valloc.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+#include "ft_printf.h"
+
+// void	*valloc(size_t size)
+// {
+// 	void	*ptr = NULL;
+
+// 	// ptr = malloc(size);
+// 	ft_printf("valloc: ptr = %p, size = %d\n", ptr, size);
+// 	return (ptr);
+// }
+// int 	 posix_memalign(void **memptr, size_t alignment, size_t size)
+// {
+// 	ft_printf("posix: ptr = %p, size = %d\n", memptr, size);
+// 	alignment = 0;
+// 	return alignment;
+// }
