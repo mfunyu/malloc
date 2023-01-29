cdd6acbba5ebcb21b52747cd60e16295426f0079
mfunyu <mfunyu@student.42lyon.fr>
2023-01-29 11:39:56 +0900

    update: print format

--- srcs/alloc_debug.c
+++ srcs/alloc_debug.c
@@ -7,5 +7,5 @@ void	alloc_debug(const char *func_name, void *ret_addr)
 	Dl_info	info;
 
 	dladdr(ret_addr, &info);
-	ft_printf("[%s] %s calls ", func_name, info.dli_sname);
+	ft_printf("[%s] by %s	: ", func_name, info.dli_sname);
 }
--- srcs/realloc.c
+++ srcs/realloc.c
@@ -5,8 +5,9 @@
 void	*realloc(void *ptr, size_t size)
 {
 	alloc_debug(__func__, __builtin_return_address(0));
-	ft_printf("ptr = %p, size = %d\n", ptr, size);
+	ft_printf("ptr = %p, size = %d ", ptr, size);
 	if (!ptr)
 		ptr = malloc(size);
+	ft_printf("ret = %p\n", ptr);
 	return (ptr);
 }
