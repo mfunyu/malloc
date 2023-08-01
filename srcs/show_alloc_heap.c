20dbda70cbef854a5606600e16dc18cafac80475
mfunyu <you@example.com>
2023-08-01 16:01:01 +0200

    update: show_alloc_heap v3 refactored

--- srcs/show_alloc_heap.c
+++ srcs/show_alloc_heap.c
@@ -25,50 +25,74 @@ void	print_single_line(bool line)
 	ft_putstr_fd("+\n", STDOUT_FILENO);
 }
 
-void	print_head(void *ptr, bool line)
+void	print_first_column(void *ptr)
 {
-	print_single_line(line);
-	ft_printf("%15p   ||", ptr);
+	if (ptr) {
+		ft_printf("%15p   ||", ptr);
+	} else {
+		ft_printf("%15s   ||", "");
+	}
 }
 
-void	print_head_to_end(t_malloc_chunk* head, t_malloc_chunk* tail)
+void	print_mem(t_malloc_chunk *chunk)
+{
+	t_malloc_chunk	*next;
+	bool			is_used;
+	size_t			i;
+
+	next = NEXTCHUNK(chunk);
+	is_used = IS_PREV_IN_USE(next);
+
+	print_single_line(false);
+	if (is_used) {
+		i = 0;
+		while (i < SIZE(chunk) && i < WORD) {
+			print_first_column(MEM(chunk) + i);
+			ft_printf(" %-20.8s |\n", MEM(chunk) + i);
+			i += BYTE;
+		}
+		if (i <= SIZE(chunk) - WORD) {
+			print_first_column(NULL);
+			ft_printf(" %-20s |\n", "(( abbriviated ))");
+		}
+	} else {
+		print_first_column(&(chunk->fd));
+		ft_printf(" %20p | fd\n", chunk->fd);
+		print_single_line(false);
+		print_first_column(&(chunk->bk));
+		ft_printf(" %20p | bk\n", chunk->bk);
+		if (WORD < SIZE(chunk) - WORD) {
+			print_single_line(false);
+			print_first_column((void *)&(chunk->bk) + BYTE);
+			ft_printf(" %-20s |\n", "");
+			print_first_column(NULL);
+			i = SIZE(chunk) - WORD - WORD;
+			ft_printf(" [%8d (%7p)] |\n", i, i);
+		}
+	}
+}
+
+void	print_region(t_malloc_chunk* head, t_malloc_chunk* tail)
 {
 	t_malloc_chunk	*chunk;
-	t_malloc_chunk	*next;
-	bool		is_current_inuse;
 
-	ft_printf("%p ~ %p (%d bytes)\n", head, tail, tail - head);
-	chunk = head;
 	int i = 0;
-	while (chunk < tail && i < 4)
+	chunk = head;
+	ft_printf("%p ~ %p (%d bytes)\n", head, tail, tail - head);
+	while (chunk < tail && ++i < 4)
 	{
-		i++;
-		next = NEXTCHUNK(chunk);
-		is_current_inuse = IS_PREV_IN_USE(next);
-		if (!IS_PREV_IN_USE(chunk)) {
-			print_head(chunk, true);
-			ft_printf(" %20p |", chunk->prev_size);
-			ft_printf(" %d\n", sizeof(chunk->prev_size));
-			print_head(&(chunk->size), false);
+		print_single_line(true);
+		print_first_column(chunk);
+		if (IS_PREV_IN_USE(chunk)) {
+			ft_printf(" %-20.8s |\n", chunk);
 		} else {
-			print_head(&(chunk->size), true);
+			ft_printf(" %20p | prev_size\n", chunk->prev_size);
 		}
-		ft_printf(" %16p | %d | (%d)\n", SIZE(chunk), IS_PREV_IN_USE(chunk), SIZE(chunk));
-		if (is_current_inuse) {
-			size_t i = 0;
-			while (i < ft_strlen(MEM(chunk))) {
-				print_head(MEM(chunk) + i, false);
-				ft_printf(" %-20.8s |\n", MEM(chunk) + i);
-				i += 8;
-			}
-		} else {
-			print_head(&(chunk->fd), false);
-			ft_printf(" %20p |\n", chunk->fd);
-			print_head(&(chunk->bk), false);
-			ft_printf(" %20p |\n", chunk->bk);
-		}
-
-		chunk = next;
+		print_single_line(false);
+		print_first_column(&(chunk->size));
+		ft_printf(" %6d (%7p) | %d | \n", SIZE(chunk), SIZE(chunk), IS_PREV_IN_USE(chunk));
+		print_mem(chunk);
+		chunk = NEXTCHUNK(chunk);
 	}
 	print_single_line(true);
 }
@@ -82,11 +106,11 @@ void	show_alloc_heap()
 	tiny = g_regions.tiny_region;
 	small = g_regions.small_region;
 	large = g_regions.large_region;
-	ft_printf("TINY: ");
-	print_head_to_end(tiny.blocks, (void *)tiny.blocks + tiny.map_size);
+	ft_printf("TINY: \n");
+	print_region(tiny.blocks, (void *)tiny.blocks + tiny.map_size);
 	show_free_list(tiny);
 	ft_printf("SMALL: ");
-	print_head_to_end(small.blocks, (void *)small.blocks + tiny.map_size);
+	print_region(small.blocks, (void *)small.blocks + tiny.map_size);
 	ft_printf("LARGE: %p ~ NA (NA)\n", large.blocks);
 	// print_head_to_end(g_large_head, NULL);
 }
