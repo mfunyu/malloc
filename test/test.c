de05fa4d476a75e5576232a3257849d47d60c5ef
mfunyu <you@example.com>
2023-08-01 15:01:24 +0200

    new: test

--- test/test.c
+++ test/test.c
@@ -11,22 +11,32 @@ char *set_data(size_t size, int chr)
 	return ptr;
 }
 
-void put_data(char *ptr)
-{
-	size_t size = ft_strlen(ptr);
-	ft_printf("[malloced] ptr = %p  size = %d\n	\"%s\"\n", ptr, size, ptr);
-}
-
 int main()
 {
-	char*	ptr1 = set_data(21, '*');
-	char*	ptr2 = set_data(22, '-');
-	char*	ptr3 = set_data(21, '+');
-
-	put_data(ptr1);
-	put_data(ptr2);
-	put_data(ptr3);
-
-	// ptr = realloc(ptr, 42);
-	// free(ptr);
+	/* single free check
+	 */
+	{
+		char*	ptr1;
+		ptr1 = set_data(21, '*');
+		free(ptr1);
+    	set_data(41, 'b');
+	}	
+	/* basic check
+	{
+		set_data(21, 'b');
+		set_data(45, 'B');
+		set_data(12, '*');
+	}
+	 */
+	/* double free check
+	{
+		char*	ptr1;
+		ptr1 = set_data(21, '*');
+		free(ptr1);
+		char*	ptr2 = set_data(42, '-');
+		free(ptr2);
+		char*	ptr3 = set_data(21, '+');
+		free(ptr3);
+	}	
+	 */
 }
