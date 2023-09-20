56916d732e80e755325ca76d0f3869583b8d06a4
mfunyu <you@example.com>
2023-09-20 11:55:02 +0200

    add: google test

---
+++ test/test_malloc.cc
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include "ft_printf.h"
+
+void* ft_malloc(size_t size)
+{	
+	void		*handle;
+	static void	*(*my_malloc)(size_t);
+
+	if (!my_malloc)
+	{
+		handle = dlopen("../libft_malloc.so", RTLD_LOCAL | RTLD_LAZY);
+		if (!handle) {
+			exit(EXIT_FAILURE);
+		}
+		*(void **) (&my_malloc) = dlsym(handle, "malloc");
+		printf("===== malloc loaded =====\n");
+		//dlclose(handle);
+	}
+	return my_malloc(size);
+}
+
+void ft_free(void *ptr)
+{	
+	void		*handle;
+	static void	(*my_free)(void *);
+
+	if (!my_free)
+	{
+		handle = dlopen("../libft_malloc.so", RTLD_LOCAL | RTLD_LAZY);
+		if (!handle) {
+			exit(EXIT_FAILURE);
+		}
+		*(void **) (&my_free) = dlsym(handle, "free");
+		printf("===== free loaded =====\n");
+		//dlclose(handle);
+	}
+	return my_free(ptr);
+}
+
+
+char *set_data(size_t size, int chr)
+{
+	char *ptr = (char *)ft_malloc(size + 1);
+	memset(ptr, chr, size);
+	ptr[size] = '\0';
+
+	printf("%p: %c\n", ptr, chr);
+	return ptr;
+}
+
+TEST(MallocFreeTest, Simple) {
+  char *ptr = set_data(30, 'a');
+  ft_free(ptr);
+}
