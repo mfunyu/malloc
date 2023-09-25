b29553af86a869d3bb4565dfcbeaedac166a77dd
mfunyu <you@example.com>
2023-09-25 15:11:14 +0200

    refac: return error for init

--- srcs/malloc_init.c
+++ srcs/malloc_init.c
@@ -3,7 +3,7 @@
 #include "ft_printf.h"
 #include "alloc.h"
 
-static void	_init_region(t_region *region, e_size size_type)
+static int	_init_region(t_region *region, e_size size_type)
 {
 	size_t 			map_size;
 	t_heap_chunk	*block;
@@ -21,16 +21,22 @@ static void	_init_region(t_region *region, e_size size_type)
 	region->map_size = map_size;
 	
 	block = (t_heap_chunk *)alloc_pages_by_size(map_size);
+	if (!block)
+		return (ERROR);
 	block->prev_size = 0;
 	block->size = map_size | PREV_IN_USE;
 	region->head = block;
 	region->tail = (void *)block + map_size;
 	add_chunk_to_freelist(block, &region->freelist);
+	return (SUCCESS);
 }
 
-void	init_malloc()
+int	init_malloc()
 {
+	if (_init_region(&(g_regions.tiny_region), TINY) == ERROR)
+		return (ERROR);
+	if (_init_region(&(g_regions.small_region), SMALL) == ERROR)
+		return (ERROR);
 	g_regions.initialized = true;
-	_init_region(&(g_regions.tiny_region), TINY);
-	_init_region(&(g_regions.small_region), SMALL);
+	return (SUCCESS);
 }
