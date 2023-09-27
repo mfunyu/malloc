6ddb767df7d0a1da2a95e7535e77ea644a6a016a
mfunyu <you@example.com>
2023-09-27 13:23:30 +0200

    add: show_alloc_mem

--- includes/malloc.h
+++ includes/malloc.h
@@ -74,6 +74,7 @@ typedef struct s_malloc
 extern t_malloc	g_regions;
 
 void	show_alloc_heap();
+void	show_alloc_mem();
 void	show_free_list(t_region region);
 void	add_chunk_to_freelist(t_heap_chunk *chunk, t_heap_chunk **freelist);
 
--- srcs/show_alloc_mem.c
+++ srcs/show_alloc_mem.c
@@ -1,7 +1,35 @@
 #include "malloc.h"
+#include "ft_printf.h"
 
+void	print_large(t_mmap_chunk *lst)
+{
+	while (lst)
+	{
+		ft_printf("LARGE: %p", lst);
+		ft_printf("%p ~ %p : %d bytes\n", MEM(lst), MEM(lst) + SIZE(lst), SIZE(lst));
+		lst = lst->fd;
+	}
+}
+
+void	print_simple(t_region region)
+{
+	t_heap_chunk	*chunk;
+
+	chunk = region.head;
+	ft_printf("%p\n", region.head);
+	while ((void *)chunk < region.tail)
+	{
+		if (IS_ALLOCED(chunk))
+			ft_printf("%p ~ %p : %d bytes\n", MEM(chunk), MEM(chunk) + SIZE(chunk), SIZE(chunk));
+		chunk = NEXTCHUNK(chunk);
+	}
+}
 
 void	show_alloc_mem()
 {
-	
+	ft_printf("TINY: ");
+	print_simple(g_regions.tiny_region);
+	ft_printf("SMALL: ");
+	print_simple(g_regions.small_region);
+	print_large(g_regions.large_lst);
 }
