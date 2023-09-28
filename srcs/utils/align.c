d7945c33ef7569e5dae89ed0109be56ad59154e8
mfunyu <mfunyu@student.42lyon.fr>
2023-09-28 12:53:36 +0200

    update: alignment size

--- srcs/utils/align.c
+++ srcs/utils/align.c
@@ -9,11 +9,3 @@ size_t	align_size(size_t size)
 {
 	return align(size, BYTE);
 }
-
-size_t align_chunk_size(size_t size)
-{
-	if (size < MINSIZE)
-		size = MINSIZE;
-	size += HEADER_SIZE;
-	return align(size, MALLOC_ALIGNMENT);
-}
