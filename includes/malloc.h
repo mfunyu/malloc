c505f924071265d27a145a220a2c81cc3a6f7680
mfunyu <mfunyu@student.42lyon.fr>
2023-10-09 17:09:36 +0200

    add: malloc

--- includes/malloc.h
+++ includes/malloc.h
@@ -1,103 +1,60 @@
 #ifndef MALLOC_H
 # define MALLOC_H
 
-#include <stdbool.h>
-#include <stdint.h>
-#include <stddef.h>
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdint.h>
 
-#include "debug.h"
+# ifdef BONUS
+#  include "bonus.h"
+# endif
 
-#define APPROX_PAGE_SIZE 4096
-#define MALLOC_ABSOLUTE_SIZE_MAX (SIZE_MAX - (2 * APPROX_PAGE_SIZE))
+# define APPROX_PAGE_SIZE 4096
+# define MALLOC_ABSOLUTE_SIZE_MAX (SIZE_MAX - (2 * APPROX_PAGE_SIZE))
+# define TINY_MAX 1008
+# define SMALL_MAX 127 * 8192
 
-#define MALLOC_ALIGNMENT 16
-#define BYTE 8
-#define WORD 16
 
-#define TINY_MAX 1008
-#define SMALL_MAX 127 * 8192
-#define MIN_BLOCKS 100
 
-#define HEADER_SIZE BYTE
-# define FOOTER WORD
-# define MINSIZE 24
-# define ALLOCED 1
-# define PREV_IN_USE 2
-# define MAPPED 4
+# define FOOTERSIZE sizeof(t_malloc_footer *)
 
-# define PUT(ptr, value) *ptr = value
-# define CHUNKSIZE(chunk) (chunk->size & ~MAPPED & ~PREV_IN_USE & ~ALLOCED)
-# define ALLOCSIZE(chunk) (CHUNKSIZE(chunk) - HEADER_SIZE)
-# define IS_PREV_IN_USE(chunk) (chunk->size & PREV_IN_USE)
-# define IS_ALLOCED(chunk) (chunk->size & ALLOCED)
-# define IS_MAPPED(chunk) (chunk->size & MAPPED)
-# define IS_FOOTER(chunk) (CHUNKSIZE(chunk) == 0 && IS_ALLOCED(chunk))
-# define ALLOC(ptr, value) *(unsigned int *)(ptr + BYTE) = value
 
-# define MEM(chunk) (void *)chunk + WORD
-# define CHUNK(mem) ((t_heap_chunk *)(mem - WORD))
-# define NEXTCHUNK(chunk) ((t_heap_chunk *)((void *)chunk + CHUNKSIZE(chunk)))
-# define PREVPTR(ptr) (unsigned int **)(ptr + WORD + BYTE)
-
-typedef enum s_size
+typedef enum s_bitflag
 {
-	TINY,
-	SMALL,
-	LARGE
-}			e_size;
+	ALLOCED = 1 << 0,		// 1
+	PREV_IN_USE = 1 << 1,	// 2
+	MAPPED = 1 << 2,		// 4
+}			e_bitflag;
 
-typedef struct s_heap_chunk
+typedef struct s_malloc_chunk
 {
 	size_t					prev_size;
 	size_t					size;
-	struct s_heap_chunk 	*fd;
-	struct s_heap_chunk 	*bk;
-}				t_heap_chunk;
+	struct s_malloc_chunk	*fd;
+	struct s_malloc_chunk	*bk;
+}			t_malloc_chunk;
 
-typedef struct s_mmap_chunk
-{
-	struct s_mmap_chunk	*fd;
-	size_t				size;
-}				t_mmap_chunk;
-
-typedef struct s_footer_chunk
+typedef struct s_malloc_footer
 {
 	size_t			prev_size;
 	size_t			size;
-	t_heap_chunk	*next_chunk;
-}				t_footer_chunk;
+	t_malloc_chunk	*fd;
+}			t_malloc_footer;
 
-typedef struct s_region
+typedef struct s_magazine
 {
-	size_t			map_size;
-	void			*tail;
-	t_heap_chunk	*head;
-	t_heap_chunk	*freelist;
-}				t_region;
+	size_t			size;
+	t_malloc_chunk	*regions;
+	t_malloc_chunk	*freelist;
+}				t_magazine;
 
-typedef struct s_malloc
+typedef	struct s_malloc
 {
-	bool		initialized;
-	t_region	tiny_region;
-	t_region	small_region;
-	t_mmap_chunk	*large_lst;
+	bool		is_initialized;
+	t_magazine	tiny_magazine;
+	t_magazine	small_magazine;
 }			t_malloc;
 
-extern t_malloc	g_regions;
+extern t_malloc	g_malloc;
 
-void	show_alloc_heap();
-void	show_alloc_mem();
-void	show_free_list(t_region region);
-void	add_chunk_to_freelist(t_heap_chunk *chunk, t_heap_chunk **freelist);
-
-int		init_malloc();
-
-void	freelst_add_front(t_heap_chunk **lst, t_heap_chunk *new_chunk);
-void	freelst_insert(t_heap_chunk *prev, t_heap_chunk *new_chunk);
-void	freelst_replace(t_heap_chunk *old, t_heap_chunk *new_chunk, t_heap_chunk **head);
-void	freelst_pop(t_heap_chunk *lst, t_heap_chunk **head);
-
-void	*malloc_(size_t size);
-void	free_(void *ptr);
-
-#endif
+#endif /* MALLOC_H */
