0026c8f80c45aa63ebba2aea5000d049288f4d91
mfunyu <mfunyu@student.42lyon.fr>
2023-10-09 17:33:08 +0200

    update: get_page_size

--- srcs/utils/get_page_size.c
+++ srcs/utils/get_page_size.c
@@ -1,4 +1,5 @@
-
+#include <unistd.h>
+#include "ft_printf.h"
 
 int		get_page_size()
 {
@@ -6,7 +7,7 @@ int		get_page_size()
 
 	page_size = getpagesize();
 	if (page_size == -1) {
-		ft_putendl_fd("Error: getpagesize", STDERR_FILENO);
+		ft_printf("Error: getpagesize");
 		return (-1);
 	}
 	return page_size;
