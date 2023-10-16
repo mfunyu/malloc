b2e97cee092bb69a2d6c8580fd91e79904f9ec1d
mfunyu <mfunyu@student.42lyon.fr>
2023-10-16 12:06:26 +0200

    add: test9.c

---
+++ test/correction/test9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "malloc.h"
+#include "ft_printf.h"
+
+void	main()
+{
+	ft_printf("=== test9 jsut size for space left malloc ===\n");
+
+	void	*ptr1 = malloc(16);
+	void	*ptr2 = malloc(16);
+	free(ptr1);
+	free(ptr2);
+}
