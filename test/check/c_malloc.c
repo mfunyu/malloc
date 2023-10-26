735639ef584acaada79db5d727f4ba73e48ca536
mfunyu <mfunyu@student.42lyon.fr>
2023-10-26 16:50:23 +0200

    update: c_malloc address size

--- test/check/c_malloc.c
+++ test/check/c_malloc.c
@@ -32,8 +32,32 @@ int	malloc_error()
 	printf("\n");
 }
 
+void	malloc_rec(int *arr, void *prev)
+{
+	void	*ptr;
+
+	if (arr[1])
+		ptr = malloc(arr[1]);
+	else
+		ptr = malloc(0);
+	if (prev)
+		printf("%p : %d (%d [%p])\n", prev, arr[0], ptr - prev, ptr - prev);
+	if (!arr[1])
+		return ;
+	return malloc_rec(arr + 1, ptr);
+}
+
+int malloc_address()
+{
+	int		arr[20] = {0, 1, 16, 24, 25, 32, 33, 40, 41, 48};
+
+	print_expl("malloc check return address");
+	malloc_rec(arr, NULL);
+}
+
 int	main()
 {
 	malloc_zero();
 	malloc_error();
+	malloc_address();
 }
