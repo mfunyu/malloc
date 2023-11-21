cc88d15314f2a08216b5ad85fe837b33637f8bd9
mfunyu <mfunyu@student.42lyon.fr>
2023-11-21 15:08:19 +0100

    fix: show_freelist size over index 64

--- srcs/bonus/show_freelist.c
+++ srcs/bonus/show_freelist.c
@@ -27,23 +27,48 @@ static void	print_single_line()
 	ft_putchar_fd('\n', FILENO);
 }
 
+int	largebin_index_size(size_t index)
+{
+	if (index <= 97)
+		return ((index - 48) << 6);
+	if (index <= 112)
+		return ((index - 91) << 9);
+	if (index <= 120)
+		return ((index - 110) << 12);
+	if (index <= 124)
+		return ((index - 119) << 15);
+	return ((index - 124) << 18);
+}
+
+int get_size_by_index(size_t index)
+{
+	size_t	size;
+
+	if (index < 64)
+		size = index << 4;
+	else
+		size = largebin_index_size(index);
+	return (size);
+}
 
 void	show_freelist(t_magazine magazine)
 {
 	t_malloc_chunk	*lst;
+	size_t			size;
 
 	ft_printf("< FreeList >\n");
 
 	print_single_line();
-	for (size_t i = 2; i < 128 ; i++)
+	for (size_t i = 2; i < 128; i++)
 	{
 		lst = magazine.freelist[i];
+		size = get_size_by_index(i);
 		ft_printf(" [%i]", i);
-		ft_printf(" (%i) |", i << 4);
+		ft_printf(" (%i) |", size);
 		while (lst)
 		{
 			ft_printf("-> %p |", lst);
-			if (i << 4 != CHUNKSIZE(lst))
+			if (size != CHUNKSIZE(lst))
 				D(CHUNKSIZE(lst));
 			lst = lst->fd;
 		}
