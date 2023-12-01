d84792e73f78318164d7c7824df714893ec0e5b0
mfunyu <mfunyu@student.42lyon.fr>
2023-12-01 18:00:27 +0100

    update: test3 - add free

--- test/correction/test3.c
+++ test/correction/test3.c
@@ -8,17 +8,23 @@
 
 int	main()
 {
-	void	*ptr;
+	void	*ptr[1000];
 	size_t	i;
 
 	ft_printf("\n===== test3 Large Test: Nmalloc =====\n");
 
 	i = 0;
-	for (; i < COUNT; i++)
+	for (int j = 0; i < COUNT; i++)
 	{
-		ptr = malloc(SIZE);
-		if (!ptr)
+		ptr[j] = malloc(SIZE);
+		if (!ptr[j])
 			break;
+		if (j == 1000) /* free from old to new */
+		{
+			for (int k = 0; k < j; k++)
+				free(ptr[k]);
+			j = 0;
+		}
 	}
 	ft_printf("malloc %zu times, total over %zu bytes\n", i, i * SIZE);
 }
