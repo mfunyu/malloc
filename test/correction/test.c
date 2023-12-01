39643a45d206f078b2855eee35fcc22f8d95fd06
mfunyu <mfunyu@student.42lyon.fr>
2023-12-01 14:07:34 +0100

    update: test0 cleanup

--- test/correction/test.c
+++ test/correction/test.c
@@ -15,21 +15,23 @@ int	main()
 	ptr[i++] = malloc(42);
 	ptr[i++] = malloc(100);
 	ptr[i++] = malloc(1000);
-	ptr[i++] = malloc(1024 - 1);
+	// ptr[i++] = malloc(1024 - 1);
 	ptr[i++] = malloc(1024); /* TINY_MAX */
-	ptr[i++] = malloc(1024 + 1);
+	// ptr[i++] = malloc(1024 + 1);
 	ptr[i++] = malloc(200000);
-	ptr[i++] = malloc(1040384 - 1);
+	// ptr[i++] = malloc(1040384 - 1);
 	ptr[i++] = malloc(1040384); /* SMALL_MAX */
-	ptr[i++] = malloc(1040384 + 1);
+	// ptr[i++] = malloc(1040384 + 1);
 	ptr[i++] = malloc(500000000);
-	ptr[i++] = malloc(3000000000);
+	// ptr[i++] = malloc(3000000000);
 	ptr[i++] = malloc(10000000000);
-	ptr[i++] = malloc(12500000000);
-	ptr[i++] = malloc(12530990000); /* fail after this value */
-	ptr[i++] = malloc(70000000000);
-	ptr[i++] = malloc(9223372036854775807);
-	ptr[i++] = malloc(18446744073709543423);
+	// ptr[i++] = malloc(12500000000);
+	/*
+	ptr[i++] = malloc(12530990000); // works
+	ptr[i++] = malloc(70000000000); // error
+	ptr[i++] = malloc(9223372036854775807); // error
+	ptr[i++] = malloc(18446744073709543423); // error
+	*/
 
 	ft_printf("\n - after malloc -\n");
 	show_alloc_mem();
