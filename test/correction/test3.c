1bd11eed68204960b6fe58bc08721c2ccdd65202
mfunyu <mfunyu@student.42lyon.fr>
2023-12-01 16:52:24 +0100

    add: test3

---
+++ test/correction/test3.c
@@ -0,0 +1,24 @@
+#include "malloc.h"
+#include "ft_printf.h"
+#include <string.h>
+# include <stdint.h>
+
+# define SIZE 12530990000
+# define COUNT (SIZE_MAX / SIZE)
+
+int	main()
+{
+	void	*ptr;
+	size_t	i;
+
+	ft_printf("\n===== test3 Edge-Case Test: Nmalloc =====\n");
+
+	i = 0;
+	for (; i < COUNT; i++)
+	{
+		ptr = malloc(SIZE);
+		if (!ptr)
+			break;
+	}
+	ft_printf("malloc %zu times, total over %zu bytes\n", i, i * SIZE);
+}
