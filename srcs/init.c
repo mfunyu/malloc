0283c5caf85bf1934e0d10f397e892aa2398ce9d
mfunyu <mfunyu@student.42lyon.fr>
2024-01-22 17:49:11 +0100

    refac: merge tiny and small function

--- srcs/init.c
+++ srcs/init.c
@@ -35,39 +35,25 @@ t_malloc_chunk	*init_region(size_t	size)
 	return (head);
 }
 
-static int	_init_small_magazine(t_magazine *small_magazine)
+static int	_init_magazine(t_magazine *magazine, e_size type)
 {
 	size_t			size;
 	t_malloc_chunk	*region;
 
-	size = _calc_region_size(SMALL_MAX);
+	size = 0;
+	if (type == TINY)
+		size = _calc_region_size(TINY_MAX);
+	else if (type == SMALL)
+		size = _calc_region_size(SMALL_MAX);
 	if (!size)
 		return (-1);
 	region = init_region(size);
 	if (!region)
 		return (-1);
-	small_magazine->type = SMALL;
-	small_magazine->size = size;
-	small_magazine->regions = region;
-	small_magazine->top = region;
-	return (0);
-}
-
-static int	_init_tiny_magazine(t_magazine *tiny_magazine)
-{
-	size_t			size;
-	t_malloc_chunk	*region;
-
-	size = _calc_region_size(TINY_MAX);
-	if (!size)
-		return (-1);
-	region = init_region(size);
-	if (!region)
-		return (-1);
-	tiny_magazine->type = TINY;
-	tiny_magazine->size = size;
-	tiny_magazine->regions = region;
-	tiny_magazine->top = region;
+	magazine->type = type;
+	magazine->size = size;
+	magazine->regions = region;
+	magazine->top = region;
 	return (0);
 }
 
@@ -85,9 +71,9 @@ int		init_malloc()
 # ifdef BONUS
 	set_flags_from_environment();
 # endif
-	if (_init_tiny_magazine(&(g_malloc.tiny_magazine)) == -1)
+	if (_init_magazine(&(g_malloc.tiny_magazine), TINY) == -1)
 		return (-1);
-	if (_init_small_magazine(&(g_malloc.small_magazine)) == -1)
+	if (_init_magazine(&(g_malloc.small_magazine), SMALL) == -1)
 		return (-1);
 	g_malloc.is_initialized = true;
 	return (0);
