6abeb56f21571f83caa30fdc42e0677eca92730c
mfunyu <mfunyu@student.42lyon.fr>
2024-01-23 13:13:20 +0100

    fix: limit consolidation to max block size, resolve #55

--- srcs/bonus/consolidation.c
+++ srcs/bonus/consolidation.c
@@ -1,5 +1,6 @@
 #include "malloc_internal.h"
 #include "freelist.h"
+#include "utils.h"
 
 static void	*_consolidate_prev(t_magazine	*magazine, t_malloc_chunk *chunk)
 {
@@ -27,9 +28,19 @@ static void	_consolidate_next(t_magazine	*magazine, t_malloc_chunk *chunk)
 
 t_malloc_chunk	*consolidation(t_magazine *magazine, t_malloc_chunk *chunk)
 {
-	if (!IS_ALLOCED(NEXTCHUNK(chunk)))
+	size_t	max;
+
+	max = 0;
+	if (magazine->type == TINY)
+		max = align_malloc(TINY_MAX, TINY);
+	else if (magazine->type == SMALL)
+		max = align_malloc(SMALL_MAX, SMALL);
+
+	if (!IS_ALLOCED(NEXTCHUNK(chunk)) &&
+			NEXTCHUNK(chunk)->size + CHUNKSIZE(chunk) <= max)
 		_consolidate_next(magazine, chunk);
-	if (!IS_PREV_IN_USE(chunk))
+	if (!IS_PREV_IN_USE(chunk) &&
+			PREVCHUNK(chunk)->size + CHUNKSIZE(chunk) <= max)
 		chunk = _consolidate_prev(magazine, chunk);
 	return (chunk);
 }
